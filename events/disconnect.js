edb573799868569e9dc33e9a6414d1283bebf442
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-10-10 01:41:00 -0700

    Update disconnect.js

--- events/disconnect.js
+++ events/disconnect.js
@@ -3,4 +3,9 @@ module.exports.run = (bot) => {
     console.log(`${bot.user.username} Has Been Disconnected/Restarted. At: ${new Date()}`);
     bot.owners[0].send(`${bot.user} Has Been Restarted.`)
     bot.owners[1].send(`${bot.user} Has Been Restarted.`)
+    let embed1 = new Discord.RichEmbed()
+        .setColor(`#FF0000`)
+        .setTimestamp()
+        .setDescription(`${bot.user} Disconnected`)
+    bot.channels.get("499410386492063764").send(embed1)
 }
