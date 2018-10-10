788284a63d5dacf448715156f8261e280e40e7ec
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-10-10 14:50:56 -0700

    Update disconnect.js

--- events/disconnect.js
+++ events/disconnect.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-module.exports.run = (bot) => {
+module.exports.run = async (bot) => {
     console.log(`${bot.user.username} Has Been Disconnected/Restarted. At: ${new Date()}`);
     bot.owners[0].send(`${bot.user} Has Been Restarted.`)
     bot.owners[1].send(`${bot.user} Has Been Restarted.`)
