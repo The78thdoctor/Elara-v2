c55f4fa2e59f9cce6576718e5e30c2540b001341
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-10-10 20:03:44 -0700

    Create faq.js

---
+++ commands/BotOwner/faq.js
@@ -0,0 +1,24 @@
+const { Command } = require('../../util/Commando'),
+    Discord = require('discord.js');
+module.exports = class NCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "faq",
+            memberName: "faq",
+            aliases: [],
+            examples: [`${client.commandPrefix}faq`],
+            description: "N/A",
+            group: "botowner",
+            ownerOnly: true
+        })
+    }
+    async run(message) {
+        let embed = new Discord.RichEmbed()
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .setColor(`#02FFDD`)
+        .setTimestamp()
+        .setTitle(`FAQ about the Elara Bot(s)`)
+        .setDescription(`Q: Will there ever be reminder commands\nA: Maybe in the future this bot is still in development\n\nQ: How do I get the bot to log stuff in my server?\nA: Add a \`modlogs\` channel make sure that Elara has [EMBED LINKS, SEND MESSAGES, READ MESSAGES, ATTACH FILES] permissions in the channel\n\nQ: Can I use another channel for the bot to log in?\nA: Currently no, The bot doesn't support per server configurations[Will maybe add it in the future]\n\nQ: What is Elara 2 for?\nA: Elara 2 is the bot the Dev(s) push updates to before adding it to the main bot.\n\nQ: How do I add the bot to my server?\nA: Elara [Click Here](https://discordapp.com/oauth2/authorize?client_id=455166272339181589&permissions=8&scope=bot)  Elara 2 [CLick Here](https://discordapp.com/oauth2/authorize?client_id=491635097599082497&permissions=8&scope=bot)\n\nQ: What is Elara Dev?\nA: Elara Dev is the Dev(s) Testing bot, This is a Private bot, only in a few servers, Currently you can't add the bot to any server(s)\n\nQ: What is the Invite to this server?\nA: ${this.client.options.invite}\n\nQ: How do I know what has been changed with the bot?\nA: Look in <#499410304417923074> To keep up with all of the changes :wink:`)
+        message.say(embed)
+    }
+}
