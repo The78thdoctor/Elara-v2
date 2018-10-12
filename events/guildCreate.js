bf81ae26d58390d304db73c4d760ef56683b3c18
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-10-12 16:34:27 -0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,17 @@
 const Discord = require('discord.js');
 module.exports.run = (bot, guild) => {
+     function checkDays(date) {
+            let now = new Date();
+            let diff = now.getTime() - date.getTime();
+            let days = Math.floor(diff / 86400000);
+            return days + (days == 1 ? " day" : " days") + " ago";
+        };
+    let embed = new Discord.RichEmbed()
+    .setColor(`#FF000`)
+    .setDescription(`${guild.name}**[\`[${guild.id}]\`](${guild.iconURL ? guild.iconURL : "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png"})**\nServer Created: **${guild.createdAt.toString().substr(0, 15)},\n${checkDays(guild.createdAt)}**\nWe now have **${bot.guilds.size}** Servers`)
+    .setAuthor(`Server Joined`, guild.iconURL ? guild.iconURL : "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png")
+    .setTimestamp()
+    bot.channels.get("499410445082427412").send(embed)
     require('../util/playing')(bot)
     let serverSize = guild.memberCount;
     let botCount = guild.members.filter(m => m.user.bot).size;
