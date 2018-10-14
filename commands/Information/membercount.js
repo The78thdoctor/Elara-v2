cc2c5851b266ac121c8f466d7336787033f10a71
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-10-13 23:58:28 -0700

    Update membercount.js

--- commands/Information/membercount.js
+++ commands/Information/membercount.js
@@ -20,7 +20,10 @@ module.exports = class NCommand extends Command {
             .addField(`Members`, `**${message.guild.memberCount}**`, true)
             .addField(`Humans`, `**${message.guild.members.filter(member => !member.user.bot).size}**`, true)
             .addField(`Bots`, `**${message.guild.members.filter(member => member.user.bot).size}**`, true)
-            .addField(`Member Statuses`, `**${message.guild.members.filter(o => o.presence.status === 'online').size}** Online\n**${message.guild.members.filter(i => i.presence.status === 'idle').size}** Idle/Away\n**${message.guild.members.filter(dnd => dnd.presence.status === 'dnd').size}** Do Not Disturb\n**${message.guild.members.filter(off => off.presence.status === 'offline').size}** Offline/Invisible`, true)
+         if (message.guild.members.get('248947473161256972')){
+            embed.addField(`VAL`, `1`, true)
+        }
+        embed.addField(`Member Statuses`, `**${message.guild.members.filter(o => o.presence.status === 'online').size}** Online\n**${message.guild.members.filter(i => i.presence.status === 'idle').size}** Idle/Away\n**${message.guild.members.filter(dnd => dnd.presence.status === 'dnd').size}** Do Not Disturb\n**${message.guild.members.filter(off => off.presence.status === 'offline').size}** Offline/Invisible`, true)
         message.channel.send(embed)
     }
 }
