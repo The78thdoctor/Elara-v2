5f7b958fe4bea15ace4d256d71649921877b1e99
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-10-21 03:09:24 -0700

    Update botinfo.js

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -1,6 +1,8 @@
 const { Command } = require('../../util/Commando');
 const Discord = require('discord.js');
 const config = require('../../config.js')
+const moment = require('moment')
+require('moment-duration-format')
 module.exports = class BotinfoCommand extends Command {
     constructor(client) {
         super(client, {
@@ -21,17 +23,15 @@ module.exports = class BotinfoCommand extends Command {
             .addField("❯ Prefixes", `${this.client.commandPrefix}, ${this.client.user}`)
             .addField("❯ Mention", this.client.user, true)
             .addField(`❯ Name`, this.client.user.username, true)
+            .addBlankField(false)
             .addField(`❯ Tag`, `\`${this.client.user}\``, true)
-            .addField(`❯ ID`, this.client.user.id, true)
-            .addField(`❯ Discriminator`, `#${this.client.user.discriminator}`, true)
+            .addField(`❯ ID`, `**${this.client.user.id}**`, true)
+            .addField(`❯ Discriminator`, `**#${this.client.user.discriminator}**`, true)
+            .addField("❯ Created On", `**${moment(this.client.user.createdAt).format('MMMM Do YYYY')}**`, true)
+            .addBlankField(false)
             .addField(`❯ Bot Owner(s)`, this.client.owners, true)
-            .addField("❯ Created On", "**June 9th 2018**", true)
-            .addField("❯ Invite Link", `[Click Here](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&permissions=8&scope=bot)`, true)
-            .addField(`❯ My Support Server`, `[Click Here](${this.client.options.invite})`, true)
-            .addField(`❯ Discord Bots.org Link`, `[Click Here](https://discordbots.org/bot/455166272339181589)`, true)
-            .addField(`❯ Upvote the bot`, `[Vote Here!](https://discordbots.org/bot/455166272339181589/vote)`, true)
-            .addField(`❯ Support The Bot Here!`, `[Patreon](https://www.patreon.com/Elara)`, true)
-            .addField(`❯ Servers`, `Do \`${this.client.commandPrefix}servers\` to see the full list.`, true)
+            .addField(`❯ Links`, `Bot Invite: [Click Here](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&permissions=8&scope=bot)\nSupport Server: [Click Here](${this.client.options.invite})\nDBL: [Click Here](https://discordbots.org/bot/455166272339181589)\nUpvote: [Vote Here!](https://discordbots.org/bot/455166272339181589/vote)`, true)
+            .addField(`❯ Servers`, `Do \`${this.client.commandPrefix}servers\` to see the server list`)
             .setFooter(`Requested By ${message.author.tag}`, message.author.displayAvatarURL);
         message.say(botembed)
     }
