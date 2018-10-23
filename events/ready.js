8c1dc419256882717b5e078f25c4bdff909cf7f7
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-10-23 01:41:38 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -14,4 +14,12 @@ module.exports.run = (client) => {
     .setTimestamp()
     .setDescription(`${client.user} Connected`)
     client.channels.get("499410386492063764").send(embed)
+        let embed2 = new Discord.RichEmbed()
+    .setColor(`RANDOM`)
+    .setAuthor(client.user.tag, client.user.displayAvatarURL)
+    .setTimestamp()
+    .setTitle(`Bot Successfully Connected!`)
+    .addField(`Information`,`\`\`\`ASCIIDOC\n= Bot Account: =\n[${client.user.tag}]\n= Bot ID: =\n[${client.user.id}]\n= Bot Prefix: =\n[${client.commandPrefix}]\n= Server Count: =\n[${client.guilds.size}]\n= Emoji Count: =\n[${client.emojis.size}]\n= Channel Count: =\n[${client.channels.size}]\n= User Count: =\n[${client.users.size}]\`\`\``)
+    .setFooter(`Connected to Discord Websocket At`)
+    client.channels.get(client.config.logchannel).send(embed2)
 }
