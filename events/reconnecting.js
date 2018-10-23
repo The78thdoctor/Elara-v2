2d10f3034bef5dea78f31e187185c4a92064520b
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-10-23 01:41:41 -0700

    Update reconnecting.js

--- events/reconnecting.js
+++ events/reconnecting.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot) => {
         .setFooter(`Reconnected At`)
         .setDescription(`${bot.user} Has Successfully Reconnected!`)
     console.log(`${bot.user.username} Is Trying To Reconnect! At: ${new Date()}`);
-    bot.channels.get('468372950266150916').send(embed);
+    bot.channels.get(bot.config.logchannel).send(embed);
     bot.owners[0].send(embed)
     require('../util/playing.js')(bot)
         let embed1 = new Discord.RichEmbed()
@@ -20,4 +20,5 @@ module.exports.run = async (bot) => {
     .setDescription(`${bot.user} Connected`)
    msg.channel.send(embed2)
     })
+    
 }
