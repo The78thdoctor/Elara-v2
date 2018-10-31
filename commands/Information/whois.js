2d2610712b87567213ff6ab74ff9ef23ec4b987d
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-10-31 11:09:24 -0700

    Update whois.js

--- commands/Information/whois.js
+++ commands/Information/whois.js
@@ -1,36 +1,40 @@
 const { Command } = require('../../util/Commando');
 const Discord = require('discord.js');
 const moment = require('moment-timezone')
-const perms = require('../../util/perms.json');
-const {arrayClean} = require('../../util/util.js');
+const { arrayClean } = require('../../util/util.js');
 module.exports = class WhoisCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'whois',
-            memberName: 'whois',
+            memberName: 'whoais',
             group: 'information',
             aliases: [],
-            description: 'Gets information about a user.',
-            format: 'MemberID|MemberName(partial or full)',
+            description: 'Gets information about someone',
             examples: [`${client.commandPrefix}whois @user`],
             guildOnly: true,
             throttling: {
                 usages: 2,
                 duration: 3
             },
-        args: [
-            {
-                key: 'member',
-                prompt: 'What member do you want the info about?',
-                type: 'member',
-                default: msg => msg.member
-            }
-        ]
-    })}
-    
-    async run (msg, {member}) {
-        const allowed = Object.entries(member.permissions.serialize()).filter(([perm, allowed]) => allowed).map(([perm]) => "`" + perms[perm] + "`").join(',   ');
+            args: [ 
+                {
+                    key: "member",
+                    prompt: "What member do you want the info on?",
+                    type: "member",
+                    default: msg => msg.member
+                }
+            ]
+        })
+    }
 
+    async run(msg, {member}) {
+        let types = [
+            "Playing",
+            "Streaming",
+            "Listening",
+            "Watching"
+        ]
+        let prefix = msg.guild._commandPrefix ? msg.guild._commandPrefix : this.client.commandPrefix;
         let embed = new Discord.RichEmbed()
             .setColor(`RANDOM`)
             .setThumbnail(member.user.displayAvatarURL)
@@ -43,17 +47,17 @@ module.exports = class WhoisCommand extends Command {
             .addField(`❯\u2000\Discriminator`, `#${member.user.discriminator}`, true)
             .addField(`❯\u2000\Bot`, member.user.bot ? 'Yes' : 'No', true)
             .addField(`❯\u2000\Bot Owner`, `${member.user.id !== "288450828837322764" && member.user.id !== "391529339214364674" && member.user.id !== "440810964061913119" ? "No" : "Yes, Hi Boss <:SmileyHearts:485361754633797654>"}`, true)
-            .addField("❯\u2000\Status", member.presence.status.toUpperCase(), true)
-            .addField(`❯\u2000\Game`, member.presence.game ? member.presence.game.name : 'N/A', true)
             .addField(`❯\u2000\Avatar URL`, `[Click Here](${member.user.displayAvatarURL})`)
             .addBlankField()
             .addField(`❯\u2000\Account Created At`, `${moment(member.user.createdAt).format('dddd, MMMM Do YYYY')}\n${moment(member.user.createdAt).format('h:mm:ss a zz')}`, true)
             .addField(`❯\u2000\Joined Server At`, `${moment(member.joinedAt).format('dddd, MMMM Do YYYY')}\n${moment(member.joinedAt).format('h:mm:ss a zz')}`, true)
-            .addBlankField()
-            .addField('❯\u2000\Highest Role', member.roles.size > 1 ? member.highestRole : 'N/A', true)
-            .addField('❯\u2000\Highest Role Hoisted', member.highestRole.hoist ? "Yes" : "No", true)
-            .addField(`❯\u2000\**Permissions**`, allowed ? `•\u2000${allowed}` : '•\u2000\None')
-            .addField(`❯\u2000\Role(s)`, member.roles.size > 1 ? arrayClean(null, member.roles.map((r) => {if (r.name !== '@everyone') {return r;}return null;})).join(' | ') : 'None', false)
-        msg.say(embed)
+            if (member.presence.game !== null) { embed.addField(`❯\u2000\Playing Status`, `**Status:** ${member.presence.status.toUpperCase()}\n**Game:** ${member.presence.game ? member.presence.game.name : 'N/A'}\n**Type:** ${types[member.presence.game.type]}`, true) }
+            embed.addBlankField()
+            embed.addField('❯\u2000\Highest Role', member.roles.size > 1 ? member.highestRole : 'N/A', true)
+            embed.addField('❯\u2000\Highest Role Hoisted', member.highestRole.hoist ? "Yes" : "No", true)
+            embed.addField(`❯\u2000\Permissions`, `Do \`${prefix}myperms\``)
+            embed.addField(`❯\u2000\Role(s)`, member.roles.size > 1 ? arrayClean(null, member.roles.map((r) => {if (r.name !== '@everyone') {return r;}return null;})).join(' | ') : 'None', false)
+            embed.setFooter(`Want to see another members info? Do ${msg.guild._commandPrefix ? msg.guild._commandPrefix : this.client.commandPrefix}whois @user/userid`)
+            msg.say(embed)
     }
 }
