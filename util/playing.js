769f3ef02af6426964fadcdc574ffaa401fbe43a
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-04 09:05:42 -0800

    Update playing.js

--- util/playing.js
+++ util/playing.js
@@ -1,13 +1,13 @@
 module.exports = (bot) => {
-    bot.user.setActivity(`@${bot.user.username}help`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
+    bot.user.setActivity(`@${bot.user.username} help`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
     let userssize = bot.users.size;
     let channelsize = bot.channels.size;
     let guildsize = bot.guilds.size;
     var status = [
-        `@${bot.user.username}help | Serving: ${guildsize} Servers, ${channelsize} Channels, ${userssize} Users`,
-        `@${bot.user.username}help | More Updates Coming Soon!`,
-        `@${bot.user.username}help | My Support Server: ${bot.options.invite}`,
-        `@${bot.user.username}help | My Website: https://elara.netlify.com/`
+        `@${bot.user.username} help | Serving: ${guildsize} Servers, ${channelsize} Channels, ${userssize} Users`,
+        `@${bot.user.username} help | More Updates Coming Soon!`,
+        `@${bot.user.username} help | My Support Server: ${bot.options.invite}`,
+        `@${bot.user.username} help | My Website: https://elara.netlify.com/`
     ];
     setInterval(() => {
         let gameval = Math.floor((Math.random() * status.length));
