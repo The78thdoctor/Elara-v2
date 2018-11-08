7020fcbb950d3b78bd109f44a7f584be9f2e82a8
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-08 09:37:30 -0800

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,13 +1,20 @@
 const Discord = require('discord.js');
-module.exports.run = (bot, guild, user) => {
+module.exports.run = async (bot, guild, user) => {
     let modlogs = guild.channels.find(c => c.name === bot.util.modlogs);
     if (!modlogs) return;
-    let botembed = new Discord.RichEmbed()
-        .setColor("#FF0000")
-        .setAuthor('Member Banned', user.displayAvatarURL)
-        .setFooter(`ID: ${user.id}`)
+    let e = new Discord.RichEmbed()
+        .setAuthor(`Member Banned`, guild.iconURL)
+        .setColor(`#FF0000`)
+        .setTitle(`Info`)
+        .setFooter(bot.user.tag, bot.user.displayAvatarURL)
         .setTimestamp()
-        .setDescription(`${user} ${user.tag}`)
         .setThumbnail(user.displayAvatarURL)
-    modlogs.send(botembed);
+    if (guild.members.get(bot.user.id).permissions.has("VIEW_AUDIT_LOG")) {
+        let who = await guild.fetchAuditLogs().then(audit => audit.entries.first().executor)
+        let reason = await guild.fetchAuditLogs().then(audit => audit.entries.first().reason)
+        e.setDescription(`**Mention: **${user}\n**Tag: **${user.tag}\n**ID: ** ${user.id}\n**Banned By: **${who}\n**Reason: **${reason ? reason : "No Reason Provided"}`)
+    } else {
+        e.setDescription(`**Mention: **${user}\n**Tag: **${user.tag}\n**ID: ** ${user.id}`)
+    }
+    modlogs.send(e)
 }
