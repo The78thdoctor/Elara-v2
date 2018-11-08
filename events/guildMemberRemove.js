1d9b54a99bba7c60bd65fb576a82c8aeecd3a5c0
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-08 09:36:24 -0800

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 const {arrayClean} = require('../util/util.js');
 module.exports.run = (bot, member) => {
-    let modlogs = member.guild.channels.find(c => c.name === "modlogs");
+    let modlogs = member.guild.channels.find(c => c.name === bot.util.modlogs);
     if (!modlogs) return;
     let botembed = new Discord.RichEmbed()
         .setColor("#FF0000")
-        .setAuthor('❯\u2000\Member Left', member.user.displayAvatarURL)
+        .setAuthor('Member Left', member.user.displayAvatarURL)
         .setTimestamp()
-        .addField(`❯\u2000\UserInfo`, `❯\u2000\Mention: **${member.user}**\n ❯\u2000\Tag: **${member.user.tag}** \n ❯\u2000\ID: **${member.id}**`)
-        .addField(`❯\u2000\Had Role(s)`, member.roles.size > 1 ? arrayClean(null, member.roles.map((r) => {
+        .addField(`UserInfo`, `**Mention: **${member.user}\n**Tag: **${member.user.tag}\n**ID: **${member.id}`)
+        .addField(`Had Role(s)`, member.roles.size > 1 ? arrayClean(null, member.roles.map((r) => {
             if (r.name !== '@everyone') {
                 return r;
             }
