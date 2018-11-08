2eca61ca0dc6696525aa74c4ea5d58bf2fc37bda
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-08 09:43:23 -0800

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,16 +1,18 @@
 const Discord = require('discord.js');
-module.exports.run = (bot, message) => {
+module.exports.run = async (bot, message) => {
     if (message.channel.type === "dm") return;
     if (message.author.bot) return;
-    let modlogs = message.guild.channels.find(c => c.name === "modlogs");
+    let modlogs = message.guild.channels.find(c => c.name === bot.util.modlogs);
     if (!modlogs) return;
     let image = message.attachments.map(g => g.proxyURL)
-    if (message.content === 0) return;
-    let botembed = new Discord.RichEmbed()
-        .setColor("#FF0000")
-        .setTimestamp()
-        .setAuthor(`Message Deleted By ${message.author.tag}`, `${message.author.displayAvatarURL}`)
-        .setFooter(`${bot.user.tag}`, `${bot.user.displayAvatarURL}`)
-        .setDescription(`_ _►Content: **${message.content ? message.content : image}** \n ►Channel: <#${message.channel.id}> \n ►Message ID: ${message.id}`)
-    modlogs.send(botembed)
+    let embed = new Discord.RichEmbed()
+    .setColor(`#FF0000`)
+    .setTitle(`Content`)
+    .setAuthor(`Message Deleted`,message.author.displayAvatarURL)
+    .addField(`Info`, `**User:** ${message.author.tag}\n**User ID:** ${message.author.id}\n**Channel:** ${message.channel}`)
+    if(image) { 
+    embed.setImage(image.join(' '))
+    embed.setDescription(`${message.cleanContent ? message.cleanContent : image}`)
+    }
+    modlogs.send(embed)
 }
