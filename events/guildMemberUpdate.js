f6ce36adf9d55be0ae098d45cef0bdb8033686a0
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-08 09:42:10 -0800

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const {arrayClean} = require('../util/util.js');
 module.exports.run = (bot, oldMember, newMember) => {
-    let modlogs = oldMember.guild.channels.find(c => c.name === "modlogs");
+    let modlogs = oldMember.guild.channels.find(c => c.name === bot.util.modlogs);
     if (!modlogs) return;
     if (oldMember.nickname !== newMember.nickname) {
         let embed = new Discord.RichEmbed()
             .setColor(`RANDOM`)
-            .setAuthor(newMember.user.tag, newMember.user.avatarURL)
-            .setThumbnail(newMember.user.avatarURL)
+            .setAuthor(newMember.user.tag, newMember.user.displayAvatarURL)
+            .setThumbnail(newMember.user.displayAvatarURL)
             .setTitle(`Nickname Changed`)
             .addField(`Old Nickname`, `${oldMember.nickname ? `${oldMember.nickname}` : `${oldMember.user.username}`}`)
             .addField(`New Nickname`, `${newMember.nickname ? `${newMember.nickname}` : `${newMember.user.username}`}`)
@@ -19,8 +19,8 @@ module.exports.run = (bot, oldMember, newMember) => {
             let role2 = newMember.roles.size > 1 ? arrayClean(null, newMember.roles.map((r) => { if (r.name !== '@everyone') { return r; } return null; })).join(' | ') : 'None'
             let embed = new Discord.RichEmbed()
                 .setColor(`RANDOM`)
-                .setAuthor(newMember.user.tag, newMember.user.avatarURL)
-                .setThumbnail(newMember.user.avatarURL)
+                .setAuthor(newMember.user.tag, newMember.user.displayAvatarURL)
+                .setThumbnail(newMember.user.displayAvatarURL)
                 .setTitle(`Role Changed`)
                 .addField(`Old Roles`, `${role1}`)
                 .addField(`New Roles`, `${role2}`)
