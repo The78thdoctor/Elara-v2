35de9f7e8f76202ce5c8fd3938d0db6ac860dbb8
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-08 09:38:03 -0800

    Update guildBanRemove.js

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js');
-module.exports.run = (bot, guild, user) => {
-    let modlogs = guild.channels.find(c => c.name === "modlogs");
+module.exports.run = async (bot, guild, user) => {
+    let modlogs = guild.channels.find(c => c.name === bot.util.modlogs);
     if (!modlogs) return;
-    let botembed = new Discord.RichEmbed()
-        .setColor("#12FF00")
-        .setAuthor('Member Unbanned', user.displayAvatarURL)
-        .setFooter(`ID: ${user.id}`)
-        .setTimestamp()
-        .setDescription(`${user} ${user.tag}`)
-        .setThumbnail(user.displayAvatarURL)
-    modlogs.send(botembed);
+    let e = new Discord.RichEmbed()
+    .setAuthor(`Member Unbanned`, guild.iconURL)
+    .setColor(`#FF000`)
+    .setTitle(`Info`)
+    .setFooter(bot.user.tag, bot.user.displayAvatarURL)
+    .setTimestamp()
+    .setThumbnail(user.displayAvatarURL)
+    if (guild.members.get(bot.user.id).permissions.has("VIEW_AUDIT_LOG")) {
+        let who = await guild.fetchAuditLogs().then(audit => audit.entries.first().executor)
+        e.setDescription(`**Mention: **${user}\n**Tag: **${user.tag}\n**ID: ** ${user.id}\n**Unbanned By: **${who}`)
+    } else {
+        e.setDescription(`**Mention: **${user}\n**Tag: **${user.tag}\n**ID: ** ${user.id}`)
+    }
+    modlogs.send(e)
 }
