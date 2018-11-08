fe170154e499b0c99e6c9a78e386dd65ea450299
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-08 09:32:21 -0800

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports.run = (bot, guild, user) => {
-    let modlogs = guild.channels.find(c => c.name === "modlogs");
+    let modlogs = guild.channels.find(c => c.name === bot.util.modlogs);
     if (!modlogs) return;
     let botembed = new Discord.RichEmbed()
         .setColor("#FF0000")
