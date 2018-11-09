e1f5f4dd256de253853f7fcc6d288be3635e9893
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-09 07:11:05 -0800

    Update servers.js

--- commands/Information/servers.js
+++ commands/Information/servers.js
@@ -12,13 +12,12 @@ module.exports = class NCommand extends Command {
         })
     }
     async run(message) {
-//         let string = '';
-//         this.client.guilds.forEach(guild => { string += `${guild.name}` + '\n'; })
-//         const hastEmb = new Discord.RichEmbed()
-//             .setColor(`RANDOM`)
-//             .setDescription(string)
-//             .setTitle(`Current Number of Servers **[${this.client.guilds.size}]**\nHere is a List of all of the Servers: `)
-//         message.embed(hastEmb)
-        message.say(`This command is currently disabled.`)
+        let string = '';
+        this.client.guilds.forEach(guild => { string += `${guild.name}` + '\n'; })
+        const e = new Discord.RichEmbed()
+            .setColor(`RANDOM`)
+            .setDescription(string)
+            .setTitle(`Current Number of Servers **[${this.client.guilds.size}]**\nHere is a List of all of the Servers: `)
+        message.embed(e)
     }
 }
