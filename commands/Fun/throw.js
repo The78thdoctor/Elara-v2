e5dedc2642c88020a0dd9f98e4da77b5993b86a0
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-11 12:59:26 -0800

    Update throw.js

--- commands/Fun/throw.js
+++ commands/Fun/throw.js
@@ -20,11 +20,11 @@ module.exports = class NCommand extends Command {
     }
     async run(message, {user}) {
         const throwuser = user
-        let replies = [`Banana`, `Car`, `Truck`, `A Building`,
+        let replies = [`Banana`,`Bananas`, `Car`, `Truck`, `A Building`,
             `SpongeBob`, 'Patrick', 'Nothing', 'Admins', 'Moderators', 'Staff Members',
             'Black Hole', 'Scams', `Love`, `Hate`, `iPhone`, `Brick`, `Bad Bots`, `Chair`,
-            `Lemons`, `Cake`, `Pringles`, `Gummy Bears`, `Bus`, `Train`, `Yourself`, `Knife`, `UR MOM`,
-            `SUPERCHIEFYT`, `Babies`, `Chaz`, `VAL`, `SobSyphix`, `Cats`, `Dogs`, `Poop`, `Memes`, `Youtube Streamer`, `Twitch Streamer`,
+            `Lemons`, `Cake`, `Pringles`, `Gummy Bears`, `Bus`, `Train`, `Yourself`,
+            `SUPERCHIEFYT`, `VAL`, `SobSyphix`, `Cats`, `Dogs`, `Poop`, `Memes`, `Youtube Streamer`, `Twitch Streamer`,
             `CaittlinandPJ`, `PJ`, `Sirred90`, `Nev`
         ]
         let result = Math.floor((Math.random() * replies.length));
