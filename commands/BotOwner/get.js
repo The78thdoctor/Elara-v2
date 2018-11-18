aa5374cbd85e4b4caeb1b5e1ee1b8be402b788b7
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-11-17 16:43:36 -0800

    Update get.js

--- commands/BotOwner/get.js
+++ commands/BotOwner/get.js
@@ -29,11 +29,12 @@ module.exports = class NCommand extends Command {
             .addField(`Username`, user.username, true)
             .addField(`User ID`, user.id, true)
         if(this.client.owners.map(c => c.id).includes(user.id)){
-            e.addField(`Bot Owner`, `Yes, Hi Boss <:SmileyHearts:485361754633797654>`, true)
+            e.addField(`Bot Owner`, `Hi Boss <:SmileyHearts:485361754633797654>`, true)
         }
         if(server.size > 1) {
             e.addField(`Mutual Servers Count`, server.size, true)
-            e.addField(`Mutual Servers`, server.map(c => `**Guild: **[${c.name}]\n**ID: **(${c.id})`), false)
+            e.setTitle(`Mutual Serevers`)
+            e.setDescription(server.map(c => `**Guild: **[${c.name}]\n**ID: **(${c.id})`), false)
         }else{
             e.addField(`Mutual Server Count`, `0`, true)
             e.addField(`Mutual Servers`, `None`, true)
