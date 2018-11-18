ea02301c5e11cb0ad414287a6cc62641c18dc4b6
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-11-17 20:23:09 -0800

    Update emojis.js

--- commands/Information/emojis.js
+++ commands/Information/emojis.js
@@ -14,19 +14,54 @@ module.exports = class AvatarCommand extends Command {
             });
     }
     async run(msg) {
-        let animEmotes = [],
-            staticEmotes = [];
-
-        msg.guild.emojis.forEach((e) => {
-            e.animated ? animEmotes.push(`<a:${e.name}:${e.id}>`) : staticEmotes.push(`<:${e.name}:${e.id}>`);
-        });
-        staticEmotes = staticEmotes.length !== 0 ? `__**[${staticEmotes.length}] Normal Emotes**__\n${staticEmotes.join('')}` : '';
-        animEmotes = animEmotes.length !== 0 ? `\n\n__**[${animEmotes.length}] Animated Emotes**__\n${animEmotes.join('')}` : '';
-        let botembed = new Discord.RichEmbed()
-            .setColor(`RANDOM`)
-            .setDescription(staticEmotes + animEmotes)
-            .setAuthor(`${msg.guild.name} Emojis`, msg.guild.iconURL)
-            .setTimestamp()
-        msg.embed(botembed)
+        let guild = msg.guild;
+        let fields = []
+        let emojis = {
+            0: []
+        }
+        let counter = 0
+        guild.emojis.forEach((emoji, index) => {
+            if (emojis[counter].join('\n').length > 950) {
+                if (++index === guild.emojis.length) {
+                    emojis[counter].push(`${emoji}`)
+                } else {
+                    counter++
+                    emojis[counter] = []
+                }
+            } else {
+                emojis[counter].push(`${emoji}`)
+            }
+        })
+        if (emojis[0].join('').length !== 0) {
+            Object.keys(emojis).forEach((collection, index) => {
+                if (index !== 0) {
+                    fields.push({
+                        name: 'Continued',
+                        value: emojis[index].join(' ')
+                    })
+                } else {
+                    fields.push({
+                        name: 'Emojis',
+                        value: emojis[index].join(' ')
+                    })
+                }
+            })
+        } else {
+            fields.push({
+                name: 'Emojis',
+                value: '**None**'
+            })
+        }
+        msg.channel.send({
+            embed: {
+                timestamp: new Date(msg.timestamp),
+                color: 0xFF000,
+                author: {
+                    name: guild.name,
+                    icon: guild.iconURL ? guild.iconURL : `http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png`
+                },
+                fields: fields
+            }
+        }).catch(() => { })
     }
 }
