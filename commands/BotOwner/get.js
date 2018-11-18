7ebc9cff238926fab42bd2dafd13f0e7fa14f297
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-11-17 16:37:25 -0800

    Create get.js

---
+++ commands/BotOwner/get.js
@@ -0,0 +1,44 @@
+const { Command } = require('../../util/Commando'),
+    Discord = require('discord.js');
+module.exports = class NCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "get",
+            memberName: "get",
+            aliases: [`getuser`],
+            examples: [`${client.commandPrefix}get @user/userid/username`],
+            description: "Bot Owner Command.",
+            group: "botowner",
+            ownerOnly: true,
+            args: [
+                {
+                    key: 'user',
+                    prompt: 'What user do you want me to check out?',
+                    type: 'user'
+                }
+            ]
+        })
+    }
+    async run(msg, { user }) {
+        let server = this.client.guilds.filter(c => c.members.get(user.id));
+        // let modperms = this.client.guilds.filter(c => c.members.get(user.id).permissions.has("MANAGE_MESSAGES"))
+        let e = new Discord.RichEmbed()
+            .setAuthor(`Information on ${user.tag}`, user.displayAvatarURL)
+            .setColor(`RANDOM`)
+            .setThumbnail(user.displayAvatarURL)
+            .setTimestamp()
+            .addField(`Username`, user.username, true)
+            .addField(`User ID`, user.id, true)
+        if(this.client.owners.map(c => c.id).includes(user.id)){
+            e.addField(`Bot Owner`, `Hi Boss <:SmileyHearts:485361754633797654>`, true)
+        }
+        if(server.size > 1) {
+            e.addField(`Mutual Servers Count`, server.size, true)
+            e.addField(`Mutual Servers`, server.map(c => `**Guild: **[${c.name}] **ID: **(${c.id})`), false)
+        }else{
+            e.addField(`Mutual Server Count`, `0`, true)
+            e.addField(`Mutual Servers`, `None`, true)
+        }
+        msg.say(e)
+    }
+}
