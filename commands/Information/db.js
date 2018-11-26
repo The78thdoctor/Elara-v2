ca4ad8916bfb2b964c76b40b0bc4b174b7d2b562
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2018-11-26 10:28:08 -0800

    Update db.js

--- commands/Information/db.js
+++ commands/Information/db.js
@@ -1,62 +1,85 @@
-const { Command } = require('../../util/Commando'),
-    Discord = require('discord.js');
+const {Command} = require('../../util/Commando');
+const {RichEmbed} = require('discord.js');
 const superagent = require('superagent');
-module.exports = class NCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: "db",
-            memberName: "db",
-            aliases: [],
-            examples: [`${client.commandPrefix}db 455166272339181589`],
-            description: "Gives you the info about a bot on the discordbots.org site",
+module.exports = class DBLCommand extends Command{
+    constructor(client){
+        super(client,{
+            name: "dbl",
+            memberName: "dbl",
+            aliases: ["dbots", "db"],
             group: "information",
+            examples: [`${client.commandPrefix}dbl @bot`, `${client.commandPrefix}dbl @user user`, `${client.commandPrefix}dbl <Mention/ID/Name> <Bot/User>`],
+            description: "Gets the information on the bot or user.",
+            ownerOnly: false,
+            guarded: false,
+            guildOnly: false,
             args: [
                 {
-                    key: 'user',
+                    key: 'bot',
                     prompt: 'What bot do you want the info on?',
                     type: 'user'
+                },
+                {
+                    key: "type",
+                    prompt: "What type do you want to search, `bot` or `user`",
+                    type: "string",
+                    default: "bot"
                 }
             ]
         })
     }
-    async run(message, { user }) {
-      try {  let { body } = await superagent
-            .get(`https://www.discordbots.org/api/bots/${user.id}`);
-        let embed = new Discord.RichEmbed()
-        .setColor(`RANDOM`)
-        .setThumbnail(`https://cdn.discordapp.com/avatars/${body.id}/${body.avatar}.png`)
-        .setAuthor(body.username, `https://cdn.discordapp.com/avatars/${body.id}/${body.avatar}.png`)
-        .setDescription(body.shortdesc)
-        .addField(`Name`, body.username, true)
-        .addField(`ID`, body.id, true)
-        .addField(`Discriminator`, `#${body.discriminator}`, true)
-        .addField(`Prefix`, body.prefix, true)
-        .addField(`Server Count`, `${body.server_count ? body.server_count : "N/A"}`, true)
-        .addField(`Shard Count`, `${body.shard_count ? body.shard_count : "N/A"}`, true)
-        .addField(`Certified Bot`, `${body.certifiedBot ? "Yes" : "No"}`, true)
-        .addField(`Total Upvotes`, `${body.points ? body.points : "N/A"}`, true)
-        .addField(`Monthly Upvotes`, `${body.monthlyPoints ? body.monthlyPoints : "N/A"}`, true)
-        .addField(`Library`, body.lib ? body.lib : "None", true)
-        .addField(`Tags`, body.tags ? body.tags : "None", true)
-        .addField(`Owner(s)`, `${body.owners.map(owner => `<@${owner}>`).join('\n')}`, true)
-        .addField(`Github`, `${body.github ? `[Click Here](${body.github})` : "N/A"}`, true)
-        .addField(`Website`, `${body.website ? `[Click Here](${body.website})` : "N/A"}`, true)
-        .addField(`DiscordBots List`, `[Click Here](https://discordbots.org/bot/${body.id})`, true)
-        .addField(`Invite`, `[Click Here](${body.invite})`, true)
-        .addField(`Support Server`, `[Click Here](https://discord.gg/${body.support})`, true)
-        message.embed(embed)
-    } catch (e) {
-        let embed = new Discord.RichEmbed()
-        .setColor(`#FF0000`)
-        .setTitle(`ERROR`)
-        .setDescription(e.stack)
-        .setTimestamp()
-        .setAuthor(message.author.tag, message.author.displayAvatarURL)
-        .addField(`Server`, `${message.guild.name} (${message.guild.id})`)
-        this.client.channels.get(this.client.config.logchannel).send(embed)
-        message.say(`That bot isn't on discord bots list site.`)
-    }
+    async run(msg, {bot, type}) {
+        if (type.toLowerCase() === "bot") {
+            if (bot.bot === false) return message.channel.send(`That isn't a bot!`)
+            let { body } = await superagent.get(`https://www.discordbots.org/api/bots/${bot.id}`)
+            console.log(body)
+            let owners = body.owners.map(owner => `<@${owner}>`).join('\n');
+            let embed = new RichEmbed()
+                .setTitle(`Short Description`)
+                .setDescription(body.shortdesc)
+                .addField(`Bot Info`, `
+                **User: **${bot}\n**Tag: **${bot.tag}\n**ID: **${bot.id}`)
+                .addField(`Site Info`, `
+                **Prefix: **${body.prefix ? body.prefix : "N/A"}
+                **Server Count: **${body.server_count ? body.server_count : "N/A"}
+                **Shard Count: **${body.shards.length ? body.shards.length : "N/A"}
+                **Monthly Upvotes: **${body.monthlyPoints ? body.monthlyPoints : "N/A"}
+                **Total Upvotes: **${body.points ? body.points : "N/A"}
+                **Library: **${body.lib ? body.lib : "N/A"}
+                **Certified Bot: **${body.certifiedBot ? "Yes": "No"}
+                **Tags: **${body.tags ? body.tags.join(', ') : "N/A"}
+                **Owners: **${owners ? owners : "None"}
+                `)
+                .addField(`Links`, `
+                **Discord Bots List: **[Click Here](https://discordbots.org/bot/${body.id})
+                **Invite: **[Click Here](${body.invite})
+                **Support Server: ** ${body.support ? `[Click Here](https://discord.gg/${body.support})` : 'None'}
+                **Github: **${body.github ? `[Click Here](${body.github})` : 'None'}
+                **Website: **${body.website ? `[Click Here](${body.website})` : 'None'}
+                `)
+                .setColor(`RANDOM`)
+                .setThumbnail(bot.displayAvatarURL)
+                .setAuthor(bot.tag, bot.displayAvatarURL)
+            msg.say(embed)
+        } else
+            if (type.toLowerCase() === "user") {
+                if (bot.bot === true) return msg.channel.send(`That is a bot! Not a user!`)
+                try {
+                    let { body } = await superagent.get(`https://www.discordbots.org/api/users/${bot.id}`)
+                    if (!body) return msg.say(`${bot.tag} isn't on <https://www.discordbots.org>`)
+                    let e = new RichEmbed()
+                        .setColor(`RANDOM`)
+                        .setAuthor(bot.tag, bot.displayAvatarURL)
+                        .setThumbnail(bot.displayAvatarURL)
+                        .setTimestamp()
+                        .setTitle(`Information on ${bot.tag}`)
+                        .addField(`User Info`, `Name: ${body.username}\nID: ${body.id}\nDiscriminator: #${body.discriminator}\nAvatar: [Click Here](${bot.displayAvatarURL})`)
+                        .addField(`Website Info`, `Website Admin: ${body.admin ? "Yes" : "No"}\n Website Moderator: ${body.webMod ? "Yes" : "No"}\nDiscord Moderator: ${body.mod ? "Yes" : "No"}\nCertified Developer: ${body.certifiedDev ? "Yes" : "No"}\nSupporter: ${body.supporter ? "Yes" : "No"}`)
+                    if (body.banner !== undefined) { e.setImage(body.banner) }
+                    msg.say(e)
+                } catch (e) {
+                    msg.say(`${bot.tag} isn't on \`discordbots.org\``)
+                }
+            }
     }
 }
-
-//https://www.discordbots.org/api/bots/
