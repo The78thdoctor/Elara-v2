284f8cc356866474b9e6f53973798abff7aa0e05
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-12-12 22:56:26 -0800

    Create Sylvester.js

---
+++ commands/ImageCommands/Sylvester.js
@@ -0,0 +1,31 @@
+const { Command } = require('../../util/Commando'),
+    Discord = require('discord.js'),
+    {Sylvester} = require('../../util/photos.js');
+module.exports = class NCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "sylvester",
+            memberName: "sylvester",
+            aliases: ["sly"],
+            examples: [`${client.commandPrefix}Sylvester`],
+            description: "Posts a Sylvester Cat Photo",
+            group: "imagecommands",
+            hidden: true
+        })
+    }
+    async run(message) {
+        let replies = Sylvester;
+        let result = Math.floor((Math.random() * replies.length));
+        let embed = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .setDescription("<a:Dots:426956230582599690> Loading, Please wait..")
+
+        message.channel.send(embed).then(message => {
+            embed.setColor("RANDOM")
+            embed.setDescription("Here's a Photo of a Sylvester :heart:")
+            embed.setImage(replies[result])
+            embed.setFooter(`Sylvester Photo ${result}/${replies.length}`)
+            message.edit(embed)
+        })
+    }
+}
