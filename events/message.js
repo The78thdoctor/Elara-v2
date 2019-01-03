4dc6bb366a1f0a2318d231f4ce49ecc8e05e2226
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-01-03 12:14:36 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -6,28 +6,38 @@ module.exports.run = async (bot, message) => {
             message.react("✅")
             message.react("❌")
     }
-    if (message.channel.name === "❔weekly-riddle-answer" || message.channel.name === "weekly-riddle-answer") {
-        let riddleanswers = message.guild.channels.find(c => c.name === "elara-log") || message.guild.channels.find(c => c.name === "🤖elara-log")
-        if(!riddleanswers) return;
-        if(message.author.bot) return;
-        let role = message.guild.roles.find(c => c.name === "Riddle Submitted").id;
-        message.member.addRole(role)
-        message.delete().catch()
-        let riddleembed = new Discord.RichEmbed()
-            .setColor(`RANDOM`)
-            .setAuthor(`${message.author.tag}`, message.author.avatarURL)
-            .setTimestamp()
-            .setFooter(`Riddle submitted at`)
-            .setTitle(`Riddle submitted`)
-            .setDescription(`${message.content}`)
-        riddleanswers.send(riddleembed)
-        let dmembed = new Discord.RichEmbed()
-            .setColor(`RANDOM`)
-            .setDescription(`Your riddle answer has been submitted\nand Have been given the **Riddle Submitted** Role,\nWhich means you can't post another answer until next week`)
-            .setTimestamp()
-            .setFooter(`Answer Submitted At`)
-            .setAuthor(message.author.tag, message.author.avatarURL)
-        message.author.send(dmembed);
-    
+     if (message.channel.name === "❔weekly-riddle-answer" || message.channel.name === "weekly-riddle-answer") {
+            if(message.author.bot) return;
+            if(message.guild.id === "273525914187333637"){
+            if(!message.member.roles.has("302515743151357962")){return message.delete().catch();}
+            }
+            let riddleanswers = message.guild.channels.find(c => c.name === "elara-log") || message.guild.channels.find(c => c.name === "🤖elara-log")
+            if(!riddleanswers) return;
+            let role = message.guild.roles.find(c => c.name === "Riddle Submitted").id;
+            if(message.member.roles.has(role)){return message.delete().catch()}
+            message.member.addRole(role)
+            message.delete().catch()
+            let riddleembed = new Discord.RichEmbed()
+                .setColor(`RANDOM`)
+                .setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL)
+                .setTimestamp()
+                .setFooter(`Riddle submitted at`)
+                .setTitle(`Riddle submitted`)
+                .setDescription(`${message.content}`)
+            riddleanswers.send(riddleembed)
+
+            //Dms the user.
+            let dmembed = new Discord.RichEmbed()
+                .setColor(`RANDOM`)
+                .setDescription(`
+                Your riddle answer has been submitted
+                You Have been given the **Riddle Submitted** Role.
+                Which means you won't be able to submit another answer until next week.`)
+                .setTimestamp()
+                .setFooter(`Answer Submitted At`)
+                .setAuthor(message.author.tag, message.author.displayAvatarURL)
+            message.author.send(dmembed);
+        
+            }
         }
     }
